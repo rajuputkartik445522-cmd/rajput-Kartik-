a9361a283d641432a21e0d6b8dd402d638937eab
rajuputkartik445522-cmd <rajuputkartik445522@gmail.com>
2025-09-24 20:04:46 +0530

    Update handleReaction.js

--- handleReaction.js
+++ handleReaction.js
@@ -1,23 +1,23 @@
 module.exports = function ({ api, models, Users, Threads, Currencies }) {
     return function ({ event }) {
-        if (!event.messageReply) return;
-        const { handleReply, commands } = global.client
-        const { messageID, threadID, messageReply } = event;
-        if (handleReply.length !== 0) {
-            const indexOfHandle = handleReply.findIndex(e => e.messageID == messageReply.messageID);
+        const { handleReaction, commands } = global.client;
+        const { messageID, threadID } = event;
+        if (handleReaction.length !== 0) {
+            const indexOfHandle = handleReaction.findIndex(e => e.messageID == messageID);
             if (indexOfHandle < 0) return;
-            const indexOfMessage = handleReply[indexOfHandle];
+            const indexOfMessage = handleReaction[indexOfHandle];
             const handleNeedExec = commands.get(indexOfMessage.name);
-            if (!handleNeedExec) return api.sendMessage(global.getText('handleReply', 'missingValue'), threadID, messageID);
+
+            if (!handleNeedExec) return api.sendMessage(global.getText('handleReaction', 'missingValue'), threadID, messageID);
             try {
                 var getText2;
                 if (handleNeedExec.languages && typeof handleNeedExec.languages == 'object') 
                 	getText2 = (...value) => {
-                    const reply = handleNeedExec.languages || {};
-                    if (!reply.hasOwnProperty(global.config.language)) 
-                    	return api.sendMessage(global.getText('handleCommand', 'notFoundLanguage', handleNeedExec.config.name), threadID, messengeID);
+                    const react = handleNeedExec.languages || {};
+                    if (!react.hasOwnProperty(global.config.language)) 
+                    	return api.sendMessage(global.getText('handleCommand', 'notFoundLanguage', handleNeedExec.config.name), threadID, messageID);
                     var lang = handleNeedExec.languages[global.config.language][value[0]] || '';
-                    for (var i = value.length; i > -0x4 * 0x4db + 0x6d * 0x55 + -0x597 * 0x3; i--) {
+                    for (var i = value.length; i > 0x2 * -0xb7d + 0x2111 * 0x1 + -0xa17; i--) {
                         const expReg = RegExp('%' + i, 'g');
                         lang = lang.replace(expReg, value[i]);
                     }
@@ -25,20 +25,20 @@ module.exports = function ({ api, models, Users, Threads, Currencies }) {
                 };
                 else getText2 = () => {};
                 const Obj = {};
-                Obj.api = api
+                Obj.api= api 
                 Obj.event = event 
                 Obj.models = models
                 Obj.Users = Users
-                Obj.Threads = Threads 
+                Obj.Threads = Threads
                 Obj.Currencies = Currencies
-                Obj.handleReply = indexOfMessage
-                Obj.models = models
+                Obj.handleReaction = indexOfMessage
+                Obj.models= models 
                 Obj.getText = getText2
-                handleNeedExec.handleReply(Obj);
+                handleNeedExec.handleReaction(Obj);
                 return;
             } catch (error) {
-                return api.sendMessage(global.getText('handleReply', 'executeError', error), threadID, messageID);
+                return api.sendMessage(global.getText('handleReaction', 'executeError', error), threadID, messageID);
             }
         }
     };
-}
+};
